2fea7b0d10cce275caf507177446c9955f47c428
GustavoRamos14 <91494506+GustavoRamos14@users.noreply.github.com>
2022-06-02 17:52:47 -0300

    Add files via upload

---
+++ projeto2-PHP/tabuada.php
@@ -0,0 +1,16 @@
+<html>
+ <head>
+  <title>Apostila PHP Progressivo</title>
+ </head>
+ <body>
+ <form action="" method="get">
+  Tabuada do:  <input type="text" name="tab" /><br />
+  <input type="submit" name="submit" value="Testar" />
+ </form> 
+ <?php 
+  $tabuada = $_GET['tab'];  
+  for($count=1; $count<=10; $count++)
+   echo $tabuada." x ".$count." = ".($tabuada*$count)."<br />";
+ ?>
+ </body>
+</html>
